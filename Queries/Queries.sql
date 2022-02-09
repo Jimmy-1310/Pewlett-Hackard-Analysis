5b5eb3dfb8df2f444890c302ea622650da19f7fd
jaime castillo garza <jaime.castillog1310@gmail.com>
2022-02-09 16:28:20 -0600

    Added Retiring info

---
+++ Queries/Queries.sql
@@ -0,0 +1,18 @@
+SELECT first_name,last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
+--Count employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
+--Create a Retiring table
+SELECT first_name,last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
