24eda221275ed00b81508bd27909ca7470e54765
jaime castillo garza <jaime.castillog1310@gmail.com>
2022-02-16 18:16:20 -0600

    Added Challenge queries

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -16,3 +16,27 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 ;
+
+--Create a list of ammount of retiring employees in each department
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Created employee info list query
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+ON e.emp_no=s.emp_no
+INNER JOIN dept_emp AS de
+ON e.emp_no=de.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date='9999-01-01');
